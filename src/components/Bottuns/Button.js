73daf919af91aa1160623de1039cd0b047849cac
chidiebereojingwa <ojingwaoji@gmail.com>
2022-04-02 13:32:25 +0100

    Converted Button to arrow function

--- src/components/Bottuns/Button.js
+++ src/components/Bottuns/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Button.css'
 
-function Button (props) {
+const Button= (props) => {
   const { variant = 'primary', children, ...rest } = props
   return (
     <button className={`button ${variant}`} {...rest}>
