33460f00ef45ca92eaa38af1439abc87ca804c59
chidiebereojingwa <ojingwaoji@gmail.com>
2022-04-03 06:25:38 +0100

    Modified input story classname

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -1,12 +1,13 @@
 
 import React from 'react'
 import Input from './Input'
+import './Input.css'
 
 export default {
   title: 'Input',
   component: Input,
 }
 
-export const Small = () => <Input seize= 'small' placeholder='Small size'/>
+export const Small = () => <Input className='small' seize= 'small' placeholder='Small size'/>
 export const Medium = () => <Input seize= 'medium' placeholder='medium size' />
 export const Large = () => <Input seize= 'large' placeholder='large size' />
